2e9576fda003fe4e11598aef5f130cc91f1033f8
AlexSaxena <saxenaalex@gmail.com>
2019-10-01 13:50:09 +0200

    removes not in use step definition

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -6,10 +6,6 @@ Then("I should see {string}") do |content|
   expect(page).to have_content content
 end
 
-Given("I fill in {string} with {string}") do |string, string2|
-  fill_in string, with: string2
-end
-
 Given("the following product exist") do |table|
   table.hashes.each do |table|
     FactoryBot.create(:product, table)
